cb9d5c7ca276f65bdad111bdb000338202e8c6da
kkww <ho0unie@gmail.com>
2017-04-27 21:31:54 +0900

    Update GoodsApp.java

--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -10,7 +10,14 @@ public class GoodsApp {
 		
 		// 상품 정보 담을 배열 생성
 		Goods[] goods = new Goods[COUNT_GOODS];
-
+		for(int i = 0; i < COUNT_GOODS; i++) {
+			String info = scanner.nextLine();
+			String[] tokens = info.split(" "); //space 분리
+			String name = tokens[0];
+			int price = Integer.parseInt(tokens[1]);
+			int countStock = Integer.parseInt(tokens[2]);
+		}
+			
 		// 상품 입력
 
 		
